3d8f45ad683067a53cfee800a26565f5cf1c3e98
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-03-27 15:18:51 +0530

    fix: capture

--- client/src/pages/capture/Capture.jsx
+++ client/src/pages/capture/Capture.jsx
@@ -1,38 +1,79 @@
-import { React, useRef } from "react";
-import Webcam from "react-webcam";
+import { React, useRef, useEffect } from "react";
 import "./capture.css";
 import { Link } from "react-router-dom";
 import { useTheme } from "../../Context/theme-context.js";
 
 function Capture() {
-	const webRef = useRef();
+	const videoRef = useRef(null);
+	const photoRef = useRef(null);
 	const { theme } = useTheme();
 
-	function showImage() {
-		console.log(webRef.current);
-	}
+	const getVideo = () => {
+		navigator.mediaDevices
+			.getUserMedia({
+				video: true,
+			})
+			.then((stream) => {
+				const video = videoRef.current;
+				video.srcObject = stream;
+				video.play();
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
+	const takePicture = () => {
+		const width = 400;
+		const height = width / (16 / 9);
+
+		const video = videoRef.current;
+
+		const photo = photoRef.current;
+
+		photo.width = width;
+
+		photo.height = height;
+
+		const ctx = photo.getContext("2d");
+
+		ctx.drawImage(video, 0, 0, width, height);
+	};
+
+	const clearImage = () => {
+		const photo = photoRef.current;
+
+		const ctx = photo.getContext("2d");
+
+		ctx.clearRect(0, 0, photo.width, photo.height);
+	};
+
+	useEffect(() => {
+		getVideo();
+	}, [videoRef]);
+
 	return (
 		<div className="capture-container mx-auto mb-8 w-4/5 md:w-2/6 p-4 rounded cursor-pointer">
-			<Webcam ref={webRef} />
+			<video ref={videoRef} className="container"></video>
 			<button
-				onClick={showImage}
+				onClick={takePicture}
 				className="hero-btn px-4 py-2 mx-auto md:mx-0 ml-4"
 				style={{
 					backgroundColor: `${theme.mode.secondaryColor}`,
 					color: `${theme.mode.bgColor}`,
 				}}
 			>
-				<Link to="">Show Image on console</Link>
+				<Link to="">Capture</Link>
 			</button>
+			<canvas className="container" ref={photoRef}></canvas>
 			<button
-				onClick={showImage}
+				onClick={clearImage}
 				className="hero-btn px-4 py-2 md:mx-0 mr-16"
 				style={{
 					backgroundColor: `${theme.mode.secondaryColor}`,
 					color: `${theme.mode.bgColor}`,
 				}}
 			>
-				<Link to="">Capture</Link>
+				<Link to="">Close</Link>
 			</button>
 		</div>
 	);
