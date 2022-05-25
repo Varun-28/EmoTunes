441fecf3c04ab717c619060faa17f74fc2ade970
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-05-25 12:09:05 +0530

    done with music player

--- client/src/components/MainContainer.js
+++ client/src/components/MainContainer.js
@@ -1,51 +1,114 @@
 import React, { useEffect } from "react";
+import { useSong } from "../Context/songDataContext/song-context";
 import "../Stylesheets/MainContainer.css";
 import { AudioList } from "./AudioList";
 
 function MainContainer() {
-  useEffect(() => {
-    const allLi = document.querySelector(".menuList").querySelectorAll("li");
+	const { songsCategory, songCategoryHandler } = useSong();
+	function songHandler(e) {
+		if (e.target.checked) {
+			songCategoryHandler(e.target.value);
+		}
+	}
+	return (
+		<div className="mainContainer">
+			<div className="menuList">
+				<label
+					className={`radio-label ${
+						songsCategory.category === "all" && "radio-checked"
+					}`}
+					htmlFor="all"
+				>
+					<input
+						type="radio"
+						id="all"
+						value="all"
+						name="video-category"
+						onChange={songHandler}
+					/>
+					All
+				</label>
 
-    function changePopularActive() {
-      allLi.forEach((n) => n.classList.remove("active"));
-      this.classList.add("active");
-    }
+				<label
+					className={`radio-label ${
+						songsCategory.category === "Happy" && "radio-checked"
+					}`}
+					htmlFor="Happy"
+				>
+					<input
+						type="radio"
+						id="Happy"
+						value="Happy"
+						name="video-category"
+						onChange={songHandler}
+					/>
+					Happy
+				</label>
+				<label
+					className={`radio-label ${
+						songsCategory.category === "Sad" && "radio-checked"
+					}`}
+					htmlFor="Sad"
+				>
+					<input
+						type="radio"
+						id="Sad"
+						value="Sad"
+						name="video-category"
+						onChange={songHandler}
+					/>
+					Sad
+				</label>
+				<label
+					className={`radio-label ${
+						songsCategory.category === "Neutral" && "radio-checked"
+					}`}
+					htmlFor="Neutral"
+				>
+					<input
+						type="radio"
+						id="Neutral"
+						value="Neutral"
+						name="video-category"
+						onChange={songHandler}
+					/>
+					Neutral
+				</label>
+				<label
+					className={`radio-label ${
+						songsCategory.category === "Angry" && "radio-checked"
+					}`}
+					htmlFor="Angry"
+				>
+					<input
+						type="radio"
+						id="Angry"
+						value="Angry"
+						name="video-category"
+						onChange={songHandler}
+					/>
+					Angry
+				</label>
+				<label
+					className={`radio-label ${
+						songsCategory.category === "Surprise" && "radio-checked"
+					}`}
+					htmlFor="Surprise"
+				>
+					<input
+						type="radio"
+						id="Surprise"
+						value="Surprise"
+						name="video-category"
+						onChange={songHandler}
+					/>
+					Surprise
+				</label>
+			</div>
 
-    allLi.forEach((n) => n.addEventListener("click", changePopularActive));
-  }, []);
-
-  return (
-    <div className="mainContainer">
-
-      <div className="menuList">
-        <ul>
-          <li>
-            <a href="#">Top Hits</a>
-          </li>
-          <li>
-            <a href="#">Happy</a>
-          </li>
-          <li>
-            <a href="#">Sad</a>
-          </li>
-          <li>
-            <a href="#">Romantic</a>
-          </li>
-          <li>
-            <a href="#">Party</a>
-          </li>
-          <li>
-            <a href="#">Bhakti</a>
-          </li>
-          <li>
-            <a href="#">Old Songs</a>
-          </li>
-        </ul>
-      </div>
-
-      <AudioList />
-    </div>
-  );
+			<AudioList />
+		</div>
+	);
 }
 
 export { MainContainer };
--- client/src/index.js
+++ client/src/index.js
@@ -6,24 +6,26 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./Context/theme-context";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import { SongProvider } from "./Context/songDataContext/song-context";
 
 const options = {
-  position: positions.BOTTOM_CENTER,
-  timeout: 4000,
-  offset: "30px",
-  transition: transitions.SCALE,
+	position: positions.BOTTOM_CENTER,
+	timeout: 4000,
+	offset: "30px",
+	transition: transitions.SCALE,
 };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <AlertProvider template={AlertTemplate} {...options}>
-          <App />
-        </AlertProvider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<BrowserRouter>
+			<ThemeProvider>
+				<SongProvider>
+					<AlertProvider template={AlertTemplate} {...options}>
+						<App />
+					</AlertProvider>
+				</SongProvider>
+			</ThemeProvider>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
-
