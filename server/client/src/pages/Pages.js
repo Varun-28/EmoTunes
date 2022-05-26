b3b6c0fbf2b7e571c11ac120e263df3ea9e3b15c
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-05-26 10:03:53 +0530

    refactor code

--- server/client/src/pages/Pages.js
+++ server/client/src/pages/Pages.js
@@ -1,5 +1,4 @@
 export { Choice } from "./choice/Choice.jsx";
-export { Capture } from "./capture/Capture.jsx";
 export { Playlist } from "./playlist/Playlist.jsx";
 export { Home } from "./home/Home.jsx";
 export { Login } from "./login/Login.jsx";
